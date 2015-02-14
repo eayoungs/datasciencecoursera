4d8b07a112f02826fbe6a5968e715daaf1bf12de
Eric Youngson <eric@successionecological.com>
2015-02-13 23:41:14 -0800

    *pollutantmean.r -Added a validating funcition to find .CSV files in the given directory... -Created an if statement to isolate the attempt to read files... -Concatenated the path and file name (with index to be iterated)

--- pollutantmean.r
+++ pollutantmean.r
@@ -2,9 +2,18 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
     dir_vect <- as.character(directory)
-    data_file <- file.path(dir_vect, fsep = .Platform$file.sep)
-    df_contents <- read.csv(data_file)
-    print(df_contents)
+    valid_files <- list.files(path = dir_vect, pattern = "\\.csv$")
+
+    i = 1
+
+    if(length(valid_files) > 0) {
+    	full_path <- paste(dir_vect, valid_files[i], sep = "")
+    	data_file <- file.path(full_path, fsep = .Platform$file.sep)
+    	df_contents <- read.csv(data_file)
+    	print(df_contents)
+    } else{
+    	print("No CSV files found")
+    }
 
         ## 'pollutant' is a character vector of length 1 indicating
         ## the name of the pollutant for which we will calculate the

8bc0e596e4ea5efd453cb485a8dd465673450525
Eric Youngson <eric@successionecological.com>
2015-02-14 00:02:39 -0800

    *pollutantmean.r -Refactored code to nest the tests and improve readability

--- pollutantmean.r
+++ pollutantmean.r
@@ -2,6 +2,8 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
     dir_vect <- as.character(directory)
+    polltnt_vect <- as.character(pollutant)
+
     valid_files <- list.files(path = dir_vect, pattern = "\\.csv$")
 
     i = 1
@@ -10,19 +12,19 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     	full_path <- paste(dir_vect, valid_files[i], sep = "")
     	data_file <- file.path(full_path, fsep = .Platform$file.sep)
     	df_contents <- read.csv(data_file)
-    } else{
+    	
+    	## 'pollutant' is a character vector of length 1 indicating
+        ## the name of the pollutant for which we will calculate the
+        ## mean; either "sulfate" or "nitrate".
+    	if(polltnt_vect == "sulfate" | polltnt_vect == "nitrate") {
+    		print("Valid")
+    	} else {
+    		print("Invalid")
+    	}
+	} else{
     	print("No CSV files found")
     }
 
-        ## 'pollutant' is a character vector of length 1 indicating
-        ## the name of the pollutant for which we will calculate the
-        ## mean; either "sulfate" or "nitrate".
-    polltnt_vect <- as.character(pollutant)
-    if(polltnt_vect == "sulfate" | polltnt_vect == "nitrate") {
-    	print("Valid")
-    } else {
-    	print("Invalid")
-    }
         ## 'id' is an integer vector indicating the monitor ID numbers
         ## to be used
 
